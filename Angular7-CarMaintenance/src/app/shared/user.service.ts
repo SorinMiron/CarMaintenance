781ab974aa5193612d4da1cec70ff20a1e3254c4
SorinMiron <mironsorin98@yahoo.com>
2020-03-24 16:21:03 +0200

    - added UI form + components for register - not connected to backend yet. - added validations

--- Angular7-CarMaintenance/src/app/shared/user.service.ts
+++ Angular7-CarMaintenance/src/app/shared/user.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+
+  formModel = this.fb.group({
+    UserName : ['', Validators.required],
+    Email : ['', Validators.email],
+    FullName : [''],
+    Passwords : this.fb.group({
+      Password : ['', [Validators.required, Validators.minLength(6)]],
+      ConfirmPassword : ['',  Validators.required]
+    }, {validator : this.comparePasswords})
+
+
+  })
+
+  comparePasswords(fb:FormGroup){
+    let confirmPasswordControl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPasswordControl.errors={passwordMismatch:true}
+    if(confirmPasswordControl.errors == null || 'passwordMismatch' in confirmPasswordControl.errors ){
+      if(fb.get('Password').value != confirmPasswordControl.value ){
+        confirmPasswordControl.setErrors({passwordMismatch : true})
+      }
+      else
+      {
+        confirmPasswordControl.setErrors(null);
+      }
+
+    }
+  }
 }
